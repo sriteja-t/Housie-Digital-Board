cf391c9a7eb35dc7de64fd725c5e9803f6f75fec
Sriteja-t <sritejat9@gmail.com>
2023-09-18 17:44:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const pickedItemElement = document.getElementById('picked-item');
+const pickButton = document.getElementById('pick-button');
+const numbersContainer = document.getElementById('numbers-container');
+const minNumber = 1; 
+const maxNumber = 90; 
+
+const pickedItems = new Set(); 
+
+
+const availableNumbers = Array.from({ length: maxNumber - minNumber + 1 }, (_, index) => index + minNumber);
+
+for (let i = minNumber; i <= maxNumber; i++) {
+    const numberElement = document.createElement('div');
+    numberElement.textContent = i;
+    numberElement.classList.add('number');
+    numberElement.addEventListener('click', () => {
+        if (!pickedItems.has(i)) {
+            pickedItems.add(i);
+            numberElement.classList.add('selected');
+        }
+    });
+    numbersContainer.appendChild(numberElement);
+}
+
+pickButton.addEventListener('click', () => {
+    if (availableNumbers.length === 0) {
+        pickedItemElement.textContent = 'Game Over';
+        pickButton.disabled = true;
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+    const randomItem = availableNumbers.splice(randomIndex, 1)[0]; 
+    pickedItemElement.textContent = randomItem;
+
+    const pickedNumberElements = document.querySelectorAll('.number');
+    for (const element of pickedNumberElements) {
+        if (parseInt(element.textContent) === randomItem) {
+            element.classList.add('selected');
+            break; 
+        }
+    }
+});
